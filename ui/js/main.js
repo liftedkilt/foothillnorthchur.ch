9f647a51cd8ca4c73dcc9affbaba58dd8cc39ae7
liftedkilt <williamdforsyth@gmail.com>
2018-09-10 21:01:06 -0700

    sf

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,10 +1,29 @@
 const vm = new Vue({
     el: '#info',
     data: {
-        info: []
-    },
-    mounted() {
-        axios.get("http://192.168.86.92:8889/playlist")
-            .then(response => { this.info = response.data[0] })
+        playlist: [{
+            "id": "vAaFfoQy6s0",
+            "title": "Song Title"
+            },
+            {
+            "id": "xJxK7xWTqYg",
+            "title": "Song Title"
+            },
+            {
+            "id": "xcJrrVphzNI",
+            "title": "Song Title"
+            },
+            {
+            "id": "xxGEUFwO2io",
+            "title": "Song Title"
+            },
+            {
+            "id": "yx5BB2J7R-8",
+            "title": "Song Title"
+            }]
+    // },
+    // mounted() {
+    //     axios.get("http://192.168.86.92:8889/playlist")
+    //         .then(response => { this.playlist = response.data[0] })
     }
 });
