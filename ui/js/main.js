f48a6c1ecab1a78238c65009c875b290c0b1e484
william.forsyth <william.forsyth@liferay.com>
2018-09-11 15:36:58 -0700

    baseline

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,36 +1,10 @@
 const vm = new Vue({
     el: '#info',
     data: {
-        playlist: [
-            {
-              "videoID": "xJxK7xWTqYg",
-              "videoTitle": "Our Great God - Fernando Ortega"
-            },
-            {
-              "videoID": "GfAZhvMG2Ew",
-              "videoTitle": "His Love Can Never Fail - Kenwood Music"
-            },
-            {
-              "videoID": "i01ZER00X70",
-              "videoTitle": "O Sing My Soul"
-            },
-            {
-              "videoID": "gkvQsz7_Ui8",
-              "videoTitle": "Creation Sings The Father's Song"
-            },
-            {
-              "videoID": "SuGDU3hAZRk",
-              "videoTitle": "Escaping the Darkness"
-            },
-            {
-              "videoID": "iia_owSfv-8",
-              "videoTitle": "We Look to You [Official Lyric Video]"
-            }
-          ]
-
-    // },
-    // mounted() {
-    //     axios.get("http://192.168.86.92:8889/playlist")
-    //         .then(response => { this.playlist = response.data[0] })
+        playlist: []
+    },
+    mounted() {
+        axios.get("http://192.168.86.92:2015/playlist")
+            .then(response => { this.playlist = response.data.videos })
     }
 });
